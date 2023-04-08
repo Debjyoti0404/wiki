5668eda1e881c7a681dde16aa3e4a73608cfa990
Debjyoti0404 <debsen2003@gmail.com>
2023-04-08 16:42:16 +0530

    implemented create new pg and searchbar

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("srch", views.srch, name="search"),
-    path("wiki/<str:name>", views.default_route, name="main_route")
+    path("create", views.create_pg, name="pg_create"),
+    path("wiki/<str:name>", views.default_route, name="entry_route")
+    
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -15,14 +15,51 @@ def default_route(request, name):
     html_content = util.md_to_html(name)
     return render(request, "encyclopedia/content.html", {
         "name": name,
+        "type": "str",
         "contents": html_content
     })
 
 def srch(request):
     if request.method == "POST":
-        text = request.POST
-        content = text['q']
+        form_content = request.POST
+        content = form_content['q']
         for item in util.list_entries():
             if content.lower() == item.lower():
                 return redirect(f"wiki/{item}")
- 
+        else:
+            possible_items = list()
+            for item in util.list_entries():
+                if content.lower() in item.lower():
+                    possible_items.append(item)
+            if len(possible_items) != 0:
+                return render(request, "encyclopedia/content.html", {
+                    "name": "Suggested pages",
+                    "type": "list",
+                    "contents": possible_items
+                })
+            else:
+                return render(request, "encyclopedia/content.html", {
+                    "name": "Error",
+                    "type": "str",
+                    "contents": "Error, No such entry exists"
+                })
+            
+def create_pg(request):
+    if request.method == "POST":
+        form_content =  request.POST
+        title = form_content['title']
+        for item in util.list_entries():
+            if title.lower() == item.lower():
+                return render(request, "encyclopedia/content.html", {
+                    "name": "Error",
+                    "type": "str",
+                    "contents": "Error, Given entry name already exists"
+                })
+        else:
+            pg_content = form_content['content']
+            util.save_entry(title, pg_content)
+            return redirect(f"wiki/{title}")
+    else:
+        return render(request, "encyclopedia/create.html", {
+            "textarea_content": ""
+        })
